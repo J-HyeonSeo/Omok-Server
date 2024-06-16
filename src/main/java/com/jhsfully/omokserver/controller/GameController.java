53d9b37dfbf0a7d492a58fca3afc9f0650623d78
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2024-06-16 11:14:45 +0900

    Chore: GameController.java 셋팅

--- src/main/java/com/jhsfully/omokserver/controller/GameController.java
+++ src/main/java/com/jhsfully/omokserver/controller/GameController.java
@@ -1,5 +1,16 @@
 package com.jhsfully.omokserver.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 오목게임을 수행하고, 검증하고, 관리하는 Controller
+ */
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/game")
 public class GameController {
 
 }
