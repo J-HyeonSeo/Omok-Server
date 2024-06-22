1bd88a85863093dd90401a61ce622bb359ecc62c
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2024-06-22 13:48:51 +0900

    Feat: Repository 정의.

--- src/main/java/com/jhsfully/omokserver/dao/PlayerRepository.java
+++ src/main/java/com/jhsfully/omokserver/dao/PlayerRepository.java
@@ -1,5 +1,12 @@
 package com.jhsfully.omokserver.dao;
 
-public interface PlayerRepository {
+import com.jhsfully.omokserver.entity.Player;
+import com.jhsfully.omokserver.entity.Room;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends CrudRepository<Player, String> {
 
 }
--- src/main/java/com/jhsfully/omokserver/dao/RoomRepository.java
+++ src/main/java/com/jhsfully/omokserver/dao/RoomRepository.java
@@ -1,5 +1,10 @@
 package com.jhsfully.omokserver.dao;
 
-public interface RoomRepository {
+import com.jhsfully.omokserver.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, String> {
 
 }
