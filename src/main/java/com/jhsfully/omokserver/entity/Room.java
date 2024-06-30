77bab9ccf520e588df83c0f3bc127445e8b8715a
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2024-06-30 16:28:07 +0900

    Fix: Redis I/O 를 위한 Board Entity 일부 수정.

--- src/main/java/com/jhsfully/omokserver/entity/Room.java
+++ src/main/java/com/jhsfully/omokserver/entity/Room.java
@@ -3,7 +3,12 @@ package com.jhsfully.omokserver.entity;
 import com.jhsfully.omokserver.type.Piece;
 import com.jhsfully.omokserver.type.State;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,17 +18,32 @@ import org.springframework.data.redis.core.RedisHash;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @RedisHash(value = "Room")
 public class Room {
 
     @Id
     private String roomId;
-    private List<String> playerIdList;
+    @Builder.Default
+    private List<String> playerIdList = new ArrayList<>();
     private String blackPlayerId;
     private String whitePlayerId;
-    private State nowState;
+    @Builder.Default
+    private State nowState = State.BLACK;
     private LocalDateTime turnedAt;
     private String winnerPlayerId;
-    private Piece[][] board = new Piece[15][15];
+    @Builder.Default
+    private List<Piece> board = initializeBoard();
+
+    private static List<Piece> initializeBoard() {
+        List<Piece> newBoard = new ArrayList<>(Arrays.asList(new Piece[15 * 15]));
+        Collections.fill(newBoard, Piece.NONE);
+        return newBoard;
+    }
+
+    public static int IX(int x, int y) {
+        return x * 15 + y;
+    }
 
 }
