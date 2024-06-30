8c3c17affd8d82e652af4c350bcf2a99fcea8448
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2024-06-30 16:25:52 +0900

    Feat: Spring Security 임시 구성.

--- src/main/java/com/jhsfully/omokserver/security/SecurityConfig.java
+++ src/main/java/com/jhsfully/omokserver/security/SecurityConfig.java
@@ -1,5 +1,24 @@
 package com.jhsfully.omokserver.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 
 }
